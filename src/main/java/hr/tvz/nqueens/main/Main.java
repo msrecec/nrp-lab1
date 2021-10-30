723c89a9a81ca9a7efb4a7fb6c421fb59a5967d4
msrecec <67606889+msrecec@users.noreply.github.com>
2021-10-30 13:29:49 +0200

    Added random position generation

--- src/main/java/hr/tvz/nqueens/main/Main.java
+++ src/main/java/hr/tvz/nqueens/main/Main.java
@@ -1,13 +1,51 @@
 package hr.tvz.nqueens.main;
 
+import java.util.Random;
+
 public class Main {
+    static final Random rand = new Random();
+
     public static void main(String[] args) {
         int N = 4;
 
+
         boolean board [][] = new boolean[N][N];
 
-        board = populate(board, N);
+        board = clear(board, N);
 
+        board = setBoard(board, N);
+
+        printPositions(board, N);
+    }
+
+    private static boolean [][] setBoard(boolean [][] board, int N) {
+        int buff = N;
+        while(true) {
+            int x = rand.nextInt(N);
+            int y = rand.nextInt(N);
+            if(!board[x][y]) {
+                board[x][y] = true;
+                buff--;
+            }
+
+            if(buff == 0) break;
+        }
+        return board;
+    }
+
+    private static boolean [][] clear(boolean [][] board, int N) {
+        int count = N;
+
+        for(int i = 0; i < N; ++i) {
+            for(int j = 0; j < N; ++j) {
+                board[i][j] = false;
+            }
+        }
+
+        return board;
+    }
+
+    private static void printPositions(boolean[][] board, int N) {
         for(int i = 0; i < N; ++i) {
             for(int j = 0; j < N; ++j) {
                 if(board[i][j]) {
@@ -18,26 +56,5 @@ public class Main {
             }
             System.out.print("\n");
         }
-
-    }
-
-    private static boolean [][] populate(boolean [][] board, int N) {
-        int count = N;
-
-        for(int i = 0; i < N; ++i) {
-            for(int j = 0; j < N; ++j) {
-                board[i][j] = false;
-            }
-        }
-
-        for(int i = 0; i < N; ++i) {
-            if(count == 0) break;
-            for(int j = 0; j < N; ++j) {
-                board[i][j] = true;
-                count--;
-            }
-        }
-
-        return board;
     }
 }
